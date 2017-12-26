7beadb4c0b9dffd8635ce59efdffb09c93ee9911
Jameson Welling <jamowelling@gmail.com>
2017-12-25 17:22:01 -0700

    implement basic functionality of invoice review screen

--- src/screens/AddInvoice/InvoiceReviewScreen.js
+++ src/screens/AddInvoice/InvoiceReviewScreen.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
 import { connect } from 'react-redux';
+
+import InvoiceItem from './InvoiceItem';
 
 class InvoiceReviewScreen extends Component <{}> {
   render() {
-    console.log('newInvoice', this.props.newInvoice);
+    const { date, vendor, invoiceNumber, items } = this.props.newInvoice;
     return (
-      <View>
-        <Text>Invoice review screen</Text>
+      <View style={{ flex: 1 }}>
+        <InvoiceItem itemType='Date' item={date} />
+        <InvoiceItem itemType='Vendor' item={vendor} />
+        <InvoiceItem itemType='Invoice No' item={invoiceNumber} />
+        <InvoiceItem itemType='Line Items' item={items} />
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <TouchableOpacity style={{
+              backgroundColor: '#efeffa',
+              padding: 15,
+              elevation: 1,
+              borderRadius: 2,
+            }}
+            onPress={() => alert('invoice confirmed!')}
+          >
+            <Text>Confirm</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
