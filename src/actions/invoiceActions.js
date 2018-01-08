34bf1b0a219f9363a4c5826a93da390d50f65787
Jameson Welling <jamowelling@gmail.com>
2018-01-08 00:06:42 -0700

    first ever recursive code, used to describe the insides of objects

--- src/actions/invoiceActions.js
+++ src/actions/invoiceActions.js
@@ -174,8 +174,21 @@ export function deleteInvoice(invoiceId, callback) {
   };
 }
 
+
 export function fetchInvoices() {
 
+  function serializeAndPrintEntries(obj) {
+    const r = Object.entries(obj);
+    r.map(entry => {
+      console.log('key: ', entry[0]);
+      console.log('type: ', typeof entry[1]);
+      console.log('value: ', entry[1]);
+      console.log('----------------------------');
+      if (typeof entry[1] === 'object' && entry[1] !== null) {
+        serializeAndPrintEntries(entry[1]);
+      }
+    });
+  }
   return function (dispatch) {
     let url = `${DB_URL}/invoices`;
     dispatch(retrieveInvoicesBegin());
@@ -192,6 +205,7 @@ export function fetchInvoices() {
       if (responseJSON.errorMessage) {
         alert(responseJSON.errorMessage);
       } else {
+        serializeAndPrintEntries(responseJSON);
         dispatch(updateInvoices(responseJSON.invoices));
       }
     })
