466533fbdf09637dffbf79a20ec2ad8f192954c8
Jameson Welling <jamowelling@gmail.com>
2018-01-08 18:44:31 -0700

    add some styling to vendor selection flow

--- src/components/VendorItem.js
+++ src/components/VendorItem.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const VendorItem = ({ vendor, onPress }) => {
+const VendorItem = ({ count, vendor, onPress }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{vendor.name}</Text>
+    <TouchableOpacity style={[
+        styles.container,
+        { backgroundColor: count % 2 === 0 ? '#ddd' : '#eee' }
+      ]}
+      onPress={onPress}
+    >
+      <Text style={styles.textStyle}>{vendor.name}</Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  container: {
+    borderBottomWidth: 0.5,
+    borderColor: 'rgba(0,0,0,0.5)'
+  },
+  textStyle: {
+    fontSize: 20,
+    padding: 7,
+    paddingLeft: 12,
+  },
 };
 
 export default VendorItem;
