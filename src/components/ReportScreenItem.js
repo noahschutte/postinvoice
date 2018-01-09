c63386e48a352513a4bd3e590bdf785b8634401b
Jameson Welling <jamowelling@gmail.com>
2018-01-09 15:41:47 -0700

    basic styling for reports view page

--- src/components/ReportScreenItem.js
+++ src/components/ReportScreenItem.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const ReportScreenItem = () => {
+const ReportScreenItem = ({ type, data }) => {
   return (
-    <View>
-      <Text>Report Screen Item</Text>
+    <View style={styles.container}>
+
+      <View style={styles.wrapper}>
+        <Text style={styles.categoryTextStyle}>{type}: </Text>
+      </View>
+
+      <View style={styles.wrapper}>
+        <Text style={styles.cogsTextStyle}>{data}</Text>
+      </View>
+
     </View>
   );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  categoryTextStyle: {
+    fontSize: 26,
+  },
+  cogsTextStyle: {
+    fontSize: 26,
+  }
 };
 
 export default ReportScreenItem;
--- src/screens/Reports/ViewReportScreen.js
+++ src/screens/Reports/ViewReportScreen.js
@@ -4,12 +4,50 @@ import { View, Text } from 'react-native';
 import ReportScreenItem from '../../components/ReportScreenItem';
 
 const ViewReportScreen = ({ reportData }) => {
+  const { startInvoiceDateRange, endInvoiceDateRange } = reportData;
   console.log('reportData: ', reportData);
   return (
-    <View>
-      <Text>Report No. {reportData.id}</Text>
+    <View style={{ flex: 1 }}>
+      <Text style={[styles.textStyle, styles.introText]}>
+        Breakdown of COGs from
+      </Text>
+      <Text style={[styles.textStyle, styles.introText]}>
+        <Text style={styles.dateStyle}>{startInvoiceDateRange} </Text> 
+        to
+        <Text style={styles.dateStyle}> {endInvoiceDateRange}</Text>
+      </Text>
+
+      <ReportScreenItem
+        type='Wine'
+        data={reportData.wineCostPercentage}
+      />
+      <ReportScreenItem
+        type='Beer'
+        data={reportData.beerCostPercentage}
+      />
+      <ReportScreenItem
+        type='Food'
+        data={reportData.foodCostPercentage}
+      />
+      <Text style={styles.textStyle}>
+        Based off invoices between those dates, as well as inventory calculated from
+        this report and this report
+      </Text>
     </View>
   );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20,
+    padding: 5,
+  },
+  introText: {
+    textAlign: 'center',
+  },
+  dateStyle: {
+    color: '#117',
+  }
 };
 
 export default ViewReportScreen;
