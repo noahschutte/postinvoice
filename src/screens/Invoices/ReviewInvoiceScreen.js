6a318f890c4a7afb594f527ec2817e996046d9e6
Jameson Welling <jamowelling@gmail.com>
2018-01-10 21:36:19 -0700

    fix erroneous rendering of empty invoice items on invoice review screen

--- src/screens/Invoices/ReviewInvoiceScreen.js
+++ src/screens/Invoices/ReviewInvoiceScreen.js
@@ -15,17 +15,47 @@ class ReviewInvoiceScreen extends Component <{}> {
     });
   }
 
+  formatDate = (date) => {
+    var d = new Date(date),
+        month = '' + (d.getUTCMonth() + 1),
+        day = '' + d.getUTCDate(),
+        year = d.getUTCFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [month, day, year].join('/');
+  }
+
+  formatAmount = (amount) => {
+    return amount.substring(0,1) === '$' ? amount.slice(1) : amount;
+  }
+
+  formatLineItems = (items) => {
+    const [last] = items.slice(-1);
+    if (last.code.name === '' || last.amount === '') items.pop();
+    return [
+      ...items.map(item => {
+        return ({
+          ...item,
+          amount: this.formatAmount(item.amount),
+        });
+      })
+    ];
+  }
+
   render() {
     const { postNewInvoice, newInvoice } = this.props;
     console.log('newInvoice: ', newInvoice);
     const { date, vendor, number, items, total } = newInvoice;
+    console.log('test: ', this.formatLineItems(items));
     return (
       <View style={{ flex: 1 }}>
-        <InvoiceSection itemType='Date' item={date} />
-        <InvoiceSection itemType='Vendor' item={vendor.name} />
-        <InvoiceSection itemType='Invoice No' item={number} />
-        <InvoiceSection itemType='Line Items' item={items} />
-        <InvoiceSection itemType='Invoice Total' item={'$'+total} />
+        <InvoiceSection sectionType='Date' sectionData={this.formatDate(date)} />
+        <InvoiceSection sectionType='Vendor' sectionData={vendor.name} />
+        <InvoiceSection sectionType='Invoice No' sectionData={number} />
+        <InvoiceSection sectionType='Line Items' sectionData={items} />
+        <InvoiceSection sectionType='Invoice Total' sectionData={'$'+total} />
         <SingleButton
           onPress={() => postNewInvoice(newInvoice, this.postNewInvoiceCallback)}
           buttonText='Confirm'
